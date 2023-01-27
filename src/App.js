3ec9ed4781c1e7ff179848270bf7ce5b239cc147
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-26 20:52:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState({
+    first_name: '',
+    last_name: '',
+    email: ''
+  })
+
+  const handleInputChange = (evt) => {
+    setInput(prev => ({ ...prev, [evt.target.name]: evt.target.value}))
+  }
+
+  const handleSubmit = async(evt) => {
+    evt.preventDefault()
+    try {
+      let result = await axios('http://localhost:8080/users', {
+        method: 'POST',
+        data: JSON.stringify(input),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      console.log(result)
+    } catch(err){
+      console.log(err)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>registrar nuevo usuario</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nombre</label>
+          <input name="first_name" value={input.first_name} onChange={handleInputChange} />
+          <label>Apellido</label>
+          <input name="last_name" value={input.last_name} onChange={handleInputChange} />
+          <label>Email</label>
+          <input name="email" value={input.email} onChange={handleInputChange} />
+          <input type="submit" />
+        </div>
+      </form>
+    </>
+  )
 }
 
-export default App;
+export default App
